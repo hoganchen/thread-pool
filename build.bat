da2496b9c7321ea5625b32785426f111ab98e02a
Hogan Chen <hogan.chen@ymail.com>
2025-01-25 12:17:40 +0800

    use mingw to compile project

---
+++ build.bat
@@ -0,0 +1,14 @@
+if exist build (
+    rmdir /s /q build
+)
+
+mkdir build
+cd build
+
+@REM 使用GCC编译
+@REM cmake ..
+@REM cmake --build . --config release
+
+@REM 使用MinGW32 Cmake编译
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_CC_COMPILER=gcc -DCMAKE_MAKE_PROGRAM=mingw32-make -G "MinGW Makefiles" ..
+mingw32-make
