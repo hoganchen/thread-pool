c964c8c4520822ea75a02d2f88e5f90cef83ebb1
Hogan Chen <hogan.chen@ymail.com>
2025-01-26 11:16:09 +0800

    add more comments

--- example/main.cpp
+++ example/main.cpp
@@ -3,7 +3,18 @@
 
 #include "../include/ThreadPool.h"
 
+/*
+C++11 生成随机数
+
+定义random_device对象
+选择随机引擎（默认、线性、梅森、斐波那契）的实现类，将random_device的随机结果传入作为种子
+选择要的分布，创建分布对象，将引擎传入作为种子，让分布对象输出随机数。
+*/
 std::random_device rd;
+/*
+std::mt19937是C++标准库中的一个伪随机数生成器类，它实现了梅森旋转算法（Mersenne Twister）。
+mt19937类是一个随机数引擎，可以生成高质量的伪随机数序列。
+*/
 std::mt19937 mt(rd());
 std::uniform_int_distribution<int> dist(-1000, 1000);
 auto rnd = std::bind(dist, mt);
@@ -59,13 +70,13 @@ int main(int argc, char *argv[])
   future1.get();
   std::cout << "Last operation result is equals to " << output_ref << std::endl;
 
-  // Submit function with return parameter 
+  // Submit function with return parameter
   auto future2 = pool.submit(multiply_return, 5, 3);
 
   // Wait for multiplication output to finish
   int res = future2.get();
   std::cout << "Last operation result is equals to " << res << std::endl;
-  
+
   pool.shutdown();
 
   return 0;
